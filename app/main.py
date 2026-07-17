06f6f432f03082d62b9a38aa56905d1d9362ad28
Alexi Dg <alexisdurangomez588@gmail.com>
2026-07-17 14:09:55 -0500

    feat: :fire: uv package mananager has been added

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {"message": "¡Servidor FastAPI funcionando!"}
+
+# Para correr: uvicorn app.main:app --reload
